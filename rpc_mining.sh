1418bf0f4cab7e0b8585bc3ac91f19a986af0bd3
thienhao78 <166480187+thienhao78@users.noreply.github.com>
2024-04-20 14:56:52 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ echo 
+
+"Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+
+
+while :; do
+    
+
+./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xm92jgtzwt7jh0spf42ze -r community-pools.mysrv.cloud:10300 -m 6 -p rpc;
+   
+
+ sleep 5;
+
+
+done
